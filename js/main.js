7f6611bc60a3145bd4e2adc34b9eef7cd34dafaf
Andrey Grachev <andreysgra@gmail.com>
2026-04-07 21:37:02 +0300

    Реализует получение данных с сервера.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
-import {getOffers} from './data';
 import {disableAdForm, enableAdForm} from './ad-form';
 import {addMarkers, initMap} from './map';
 import {disableMapFiltersForm, enableMapFilterForm} from './map-filters-form';
-
-const OFFERS_COUNT = 10;
-const offers = getOffers(OFFERS_COUNT);
+import {getData} from './api';
+import {loadFailMessage} from './messages';
+import {OFFERS_MAX_COUNT} from './const';
 
 disableMapFiltersForm();
 disableAdForm();
 
-initMap(() => {
-  enableAdForm();
+const onLoadSuccess = (data) => {
+  const offers = data.slice(0, OFFERS_MAX_COUNT);
+
   addMarkers(offers);
   enableMapFilterForm();
+};
+
+initMap(() => {
+  getData(onLoadSuccess, loadFailMessage)
+    .then(enableAdForm);
 });
-
-
